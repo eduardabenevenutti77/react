11b34e6a7be4ddffc0b59763fdd71e1c0249b3d5
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-02-29 20:59:54 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import { useState } from "react";
 import "./App.css"; // importei pra dentro do meu projeto TODO o css do arquivo app.css
 
-function App() {
-  const [cep, setCep] = useState("");
-  const [retorno, setRetorno] = useState({});
-
-  // criei um função assincrona para buscar o cep
-  async function buscaCEP() {
-    // const resultado aguarda e recebe o resultado da requisição (solução da promessa)
-    const resultado = await fetch("https://viacep.com.br/ws/01001000/json/");
-    // // const json aguarda e recebe o resultado da requisição (solução da promessa)
-    const json = await resultado.json();
-
-    setRetorno(json);
-  }
-
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <input
-        type="text"
-        name="cep"
-        id="cep"
-        value={cep}
-        onChange={(e) => setCep(e.target.value)}
-      />
-      <div>
-        <p>CEP: </p>
-        <p>Logradouro: {retorno.logradouro}</p>
-        <p>Bairro: {retorno.bairro}</p>
-        <p>Cidade: {retorno.localidade}</p>
-        <p>Estado:{retorno.localidade}</p>
-      </div>
-      <button
-        onClick={() => {
-          buscaCEP();
-        }}
-      >
-        Busca CEP
-      </button>
+    <div>
+      <BuscaCep />
     </div>
-  );
+  )
 }
 
-export default App;
+
---
+++ src/components/BuscaCep.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css"; // importei pra dentro do meu projeto TODO o css do arquivo app.css
+
+function App() {
+  const [cep, setCep] = useState("");
+  const [retorno, setRetorno] = useState({});
+
+  // criei um função assincrona para buscar o cep
+  async function buscaCEP() {
+    // const resultado aguarda e recebe o resultado da requisição (solução da promessa)
+    // const resultado = await fetch("https://viacep.com.br/ws/01001000/json/");
+    // const resultado = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+    const resultado = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    // // const json aguarda e recebe o resultado da requisição (solução da promessa)
+    const json = await resultado.json();
+
+    setRetorno(json);
+    console.log(retorno);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <input
+        type="text"
+        name="cep"
+        id="cep"
+        value={cep}
+        onChange={(e) => setCep(e.target.value)}
+      />
+      <div>
+        <p>CEP: </p>
+        <p>Logradouro: {retorno.logradouro}</p>
+        <p>Bairro: {retorno.bairro}</p>
+        <p>Cidade: {retorno.localidade}</p>
+        <p>Estado:{retorno.uf}</p>
+      </div>
+      <button
+        onClick={() => {
+          buscaCEP();
+        }}
+      >
+        Busca CEP
+      </button>
+    </div>
+  );
+}
+
+export default App;
