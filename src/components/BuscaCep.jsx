ac0940ff74b6b44c25cbf7cfb02061633eb62e4b
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-03-07 19:27:43 -0300

    update

--- src/components/BuscaCep.jsx
+++ src/components/BuscaCep.jsx
@@ -3,18 +3,15 @@ import { useState } from "react";
 export default function BuscaCep() {
   const [cep, setCep] = useState("");
   const [retorno, setRetorno] = useState({});
-
   // criei um função assincrona para buscar o cep
   async function buscaCEP() {
     // const resultado aguarda e recebe o resultado da requisição (solução da promessa)
     const resultado = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     // // const json aguarda e recebe o resultado da requisição (solução da promessa)
     const json = await resultado.json();
-
     setRetorno(json);
     console.log(retorno);
   }
-
   return (
     <div className="App">
       <h1>Hello World</h1>
@@ -26,11 +23,11 @@ export default function BuscaCep() {
         onChange={(e) => setCep(e.target.value)}
       />
       <div>
-        <p>CEP: </p>
+        <p>CEP: {retorno.cep}</p>
         <p>Logradouro: {retorno.logradouro}</p>
         <p>Bairro: {retorno.bairro}</p>
         <p>Cidade: {retorno.localidade}</p>
-        <p>Estado:{retorno.uf}</p>
+        <p>Estado: {retorno.uf}</p>
       </div>
       <button
         onClick={() => {
